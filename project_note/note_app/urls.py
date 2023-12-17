6bc2f4a7544e6ec3acbb982fbb136432b799ee8f
54ntu <chaudharysantaram8@gmail.com>
2023-12-17 17:21:18 +0545

    index.html && create.html page is updated

--- project_note/note_app/urls.py
+++ project_note/note_app/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('noteList/',views.index,name="index"),
-    
+    path('createNote/',views.create_note,name="create-note"),
 ]
--- project_note/note_app/views.py
+++ project_note/note_app/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,'notes/index.html')
+
+def create_note(request):
+    return render(request,'notes/createNote.html')
