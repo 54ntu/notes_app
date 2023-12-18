4ba2bbbc24eab4f23d2d280e45c29ef3a9a69cd8
54ntu <chaudharysantaram8@gmail.com>
2023-12-18 10:57:51 +0545

    create page is updated

--- project_note/note_app/urls.py
+++ project_note/note_app/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('noteList/',views.index,name="index"),
+    path('',views.index,name="index"),
     path('createNote/',views.create_note,name="create-note"),
 ]
