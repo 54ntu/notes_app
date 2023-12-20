10bc77a30ccb2ff378169c1031df478df1499efd
54ntu <chaudharysantaram8@gmail.com>
2023-12-20 10:17:45 +0545

    models and super user is created

--- project_note/note_app/models.py
+++ project_note/note_app/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class NoteModel(models.Model):
+    note_title = models.CharField(max_length=50,null=False, blank=False)
+    note_description= models.TextField(max_length=255, null=False,blank=False)
+
+    def __str__(self):
+        return self.note_title
+
+
+    class Meta:
+        db_table='tbl_notes'
+
+
+
